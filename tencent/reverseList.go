f3cef67021a0e3a432a94481c5376586c5567cd5
billyct <billy_allen@126.com>
2021-01-27 21:41:08 +0800

    🐰 Add reverseList

---
+++ tencent/reverseList.go
@@ -0,0 +1,39 @@
+package tencent
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	var prev *ListNode = nil
+
+	for head.Next != nil {
+		next := head.Next
+		head.Next = prev
+
+		prev = head
+		head = next
+	}
+
+	head.Next = prev
+
+	return head
+}
+
+// 递归
+func reverseListRecursion(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	cur := reverseListRecursion(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+
+	return cur
+}
---
+++ tencent/reverseList_test.go
@@ -0,0 +1,70 @@
+package tencent
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "Test_reverseList: head 1,2,3,4,5",
+			args: args{
+				head: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val: 5,
+									Next: nil,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val: 2,
+							Next: &ListNode{
+								Val: 1,
+								Next: nil,
+							},
+						},
+					},
+				},
+			},
+		},
+
+		{
+			name: "Test_reverseList: head nil",
+			args: args{
+				head: nil,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
